e4b488d0a2ecaeb6e514dd05ee9563b5acbaf39e
yenoanhnguyen98@gmail.com <yenoanhnguyen98@gmail.com>
2021-01-12 18:02:33 +0700

    add

--- app/src/main/java/com/datn/mobileapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/datn/mobileapp/ui/login/LoginActivity.kt
@@ -1,7 +1,16 @@
 package com.datn.mobileapp.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.datn.mobileapp.R
 
-class LoginActivity: AppCompatActivity(R.layout.activity_login) {
+class LoginActivity : AppCompatActivity(R.layout.activity_login) {
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val navFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        navFragment!!.childFragmentManager.fragments.forEach {
+            it.onActivityResult(requestCode, resultCode, data)
+        }
+    }
 }
