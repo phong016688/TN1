2b699698386a2f61e5179721394af3ae67d6d569
phong016688 <vophongqt1998@gmail.com>
2021-01-12 09:24:25 +0700

    add activity login

---
+++ app/src/main/java/com/datn/mobileapp/ui/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.datn.mobileapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import com.datn.mobileapp.R
+
+class LoginActivity: AppCompatActivity(R.layout.activity_login) {
+}
