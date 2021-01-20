0c4b865fee8fb8c0a3a9be6e5f020a2a45783f34
yenoanhnguyen98@gmail.com <yenoanhnguyen98@gmail.com>
2021-01-20 14:50:03 +0700

    add_mail

--- app/src/main/java/com/datn/mobileapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/datn/mobileapp/ui/login/LoginFragment.kt
@@ -15,11 +15,17 @@ import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.MainScope
 
 
 @Suppress("DEPRECATION")
@@ -27,10 +33,26 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     private val viewBinding by viewBindings(FragmentLoginBinding::bind)
     private var callbackManager: CallbackManager? = null
     private var auth: FirebaseAuth? = null
+    private var user: FirebaseUser? = null
+    private var signInClient: GoogleSignInClient? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        onclick()
+        //login gmail
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        signInClient = GoogleSignIn.getClient(requireContext(), gso)
+        viewBinding.gmailImageButton.setOnClickListener {
+            signIn()
+        }
+
+        //login facebook
         auth = Firebase.auth
+        user = auth!!.currentUser
         callbackManager = CallbackManager.Factory.create();
         viewBinding.faceBookImageButton.setReadPermissions(listOf(EMAIL))
         viewBinding.faceBookImageButton.registerCallback(
@@ -38,18 +60,54 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             object : FacebookCallback<LoginResult> {
                 override fun onSuccess(result: LoginResult?) {
                     handleFacebookAccessToken(result!!.accessToken)
-                    Log.d("AAD", result?.accessToken.toString())
+                    Log.d("AAAAAAAAAAAA", user?.displayName + "__" + user?.email + "__" + user?.uid)
                     updateUI()
                 }
 
                 override fun onCancel() {
-                    Log.d("AAA", "CCCC")
+                    Log.d(this::class.java.simpleName, "onCancel")
                 }
 
                 override fun onError(error: FacebookException?) {
-                    Log.d("AAA", error.toString())
+                    Log.d(this::class.java.simpleName, error.toString())
                 }
             })
+    }
+
+    private fun onclick() : Fragment{
+
+    }
+
+    private fun signIn() {
+        val signInIntent = signInClient?.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth!!.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(this::class.java.simpleName, "signInWithCredential:success")
+                    val user = auth!!.currentUser
+                    Log.d(
+                        this::class.java.simpleName,
+                        user?.phoneNumber + "___" + user?.displayName + "___" + user?.getEmail()
+                    )
+                    //  updateUI()
+                } else {
+                    Log.w(
+                        this::class.java.simpleName,
+                        "signInWithCredential:failure",
+                        task.exception
+                    )
+                    // ...
+                    Snackbar.make(requireView(), "Authentication Failed.", Snackbar.LENGTH_SHORT)
+                        .show()
+                    // updateUI()
+                }
+            }
     }
 
     private fun handleFacebookAccessToken(accessToken: AccessToken) {
@@ -58,41 +116,47 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             auth!!.signInWithCredential(credential)
                 .addOnCompleteListener(requireActivity()) { task ->
                     if (task.isSuccessful) {
+                        Log.d(this::class.java.simpleName, "successful")
                         updateUI()
-                        // Sign in success, update UI with the signed-in user's information
-                        Toast.makeText(
-                            requireContext(), "Authentication succes.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        Log.d("AAB", "signInWithCredential:success")
-                        //  updateUI()
                     } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w("AAC", "signInWithCredential:failure", task.exception)
+                        Log.d(
+                            this::class.java.simpleName,
+                            "signInWithCredential:failure",
+                            task.exception
+                        )
                         Toast.makeText(
                             requireContext(), "Authentication failed.",
                             Toast.LENGTH_SHORT
                         ).show()
-                        //  updateUI()
                     }
-                    // ...
                 }
         } catch (e: Exception) {
-
         }
-
     }
 
     override fun onStart() {
         super.onStart()
-        if (auth!!.currentUser != null) {
-            updateUI()
-        }
+//        if (auth!!.currentUser != null) {
+//            updateUI()
+//        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         callbackManager?.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("ABB", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("ABB", "Google sign in failed", e)
+                // ...
+            }
+        }
     }
 
     private fun updateUI() {
@@ -108,5 +172,6 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     companion object {
         const val EMAIL = "email"
+        const val RC_SIGN_IN = 2
     }
 }
